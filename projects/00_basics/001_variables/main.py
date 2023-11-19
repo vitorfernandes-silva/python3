aa2fe8edd3525b59e285f645107c6b291a6eb6aa
Vitor Fernandes Silva <vitorfernandes@estudante.ufscar.br>
2023-11-19 00:29:26 -0300

    python basics

---
+++ projects/00_basics/001_variables/main.py
@@ -0,0 +1,7 @@
+name = "Python Programmer"
+age = 27
+weight = 75
+
+print(name)
+print(age)
+print(weight)
---
+++ projects/00_basics/002_input/main.py
@@ -0,0 +1,7 @@
+name = input("Insert your name: ")
+age = int(input("Insert your age: "))
+weight = float(input("Insert your weight(kg): "))
+
+print(name)
+print(age)
+print(weight)
